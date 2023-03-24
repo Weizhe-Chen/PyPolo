b7a62b3e41df16df0c75713980eff08cd984c721
Weizhe Chen <chenweiz@iu.edu>
2023-03-24 12:14:17 -0400

    Upgraded torch version requirement. Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,53 @@
-from setuptools import setup, find_packages
+import os
+import setuptools
 
-setup(
-    name='pypolo',
-    version='0.0.4',
-    author='Weizhe (Wesley) Chen',
-    author_email='chenweiz@iu.edu',
-    license="MIT",
-    packages=find_packages(),
-    description=
-    "PyPolo: A Python Library for Robotic Information Gathering",
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
+# dependencies
+INSTALL_REQUIRES = [
+    "torch>=1.13.1",
+    "matplotlib",
+    "bezier",
+    "configargparse",
+    "scipy",
+    "SciencePlots",
+    "tqdm",
+    "scikit-image",
+]
+
+setuptools.setup(
+    name="pypolo",
+    version="0.0.4",
+    author="Weizhe (Wesley) Chen",
+    author_email="chenweiz@iu.edu",
+    description="PyPolo: A Python Library for Robotic Information Gathering",
+    long_description=open(os.path.join(root_dir, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    keywords=[
+        "robotics", "machine", "learning", "information", "gathering",
+        "planning", "acquisition", "exploration", "active"
+    ],
     python_requires='>=3.6',
+    install_requires=INSTALL_REQUIRES,
+    url="https://github.com/Weizhe-Chen/PyPolo",
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    license='MIT',
+    project_urls={
+        "Documentation": "https://pypolo.readthedocs.io",
+        "Repository": "https://github.com/Weizhe-Chen/PyPolo",
+        "Bug Tracker": "https://github.com/Weizhe-Chen/PyPolo/issues",
+        "Discussions": "https://github.com/Weizhe-Chen/PyPolo/discussions",
+    },
 )
