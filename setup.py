919688b0a0ed64870bb8c28b917007f0e6d0ca22
Weizhe Chen <chenweiz@iu.edu>
2023-03-23 21:28:23 -0400

    Replaced pyproject.toml with setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypolo',
+    version='0.0.4',
+    author='Weizhe (Wesley) Chen',
+    author_email='chenweiz@iu.edu',
+    license="MIT",
+    packages=find_packages(),
+    description=
+    "PyPolo: A Python Library for Robotic Information Gathering",
+    python_requires='>=3.6',
+)
