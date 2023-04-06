0ada30ceee4ec5e05d6fc5f4a5dc9a8519b7c854
Weizhe Chen <chenweiz@iu.edu>
2023-04-06 19:26:30 -0400

    Sensor is not in the Robot class now.

--- pypolo/robots/base_robot.py
+++ pypolo/robots/base_robot.py
@@ -2,17 +2,32 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 
-from ..sensors import BaseSensor
-
 
 class BaseRobot(ABC):
+    r"""Base class for a robot."""
 
     def __init__(self, rate: float, state: np.ndarray):
+        r"""Initializes the robot with a control rate and initial state.
+
+        Args:
+            rate (float): Control rate of the robot.
+            state (np.ndarray): Initial state of the robot.
+                Shape: (num_states, ).
+
+        """
         self.state = state
         self.rate = rate
         self.control_dt = 1.0 / rate
         self.sensing_dt = 0.0
-        self.sensors = []
+
+    def take(self, action: np.ndarray) -> None:
+        r"""Takes an action and updates the state of the robot.
+
+        Args:
+            action (np.ndarray): Action to be taken by the robot.
+
+        """
+        self.state = self.step(self.state, action)
 
     @abstractmethod
     def step(self, state: np.ndarray, action: np.ndarray) -> np.ndarray:
@@ -33,27 +48,11 @@ class BaseRobot(ABC):
 
         Args:
             state (np.ndarray): Current state of the robot.
+                Shape: (num_states, ).
             goal (np.ndarray): Goal state of the robot.
+                Shape: (num_states, ).
 
         Returns:
             np.ndarray: An error array of shape (num_actions, ).
         """
         raise NotImplementedError
-
-    def move(self, action: np.ndarray) -> None:
-        r"""Move the robot.
-
-        Args:
-            action (np.ndarray): Action to be taken by the robot.
-
-        """
-        self.state = self.step(self.state, action)
-
-    def add_sensor(self, sensor: BaseSensor) -> None:
-        r"""Add a sensor to the robot.
-
-        Args:
-            sensor (BaseSensor): Sensor to be added to the robot.
-
-        """
-        self.sensors.append(sensor)
